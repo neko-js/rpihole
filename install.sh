cffa9ed518f0d8c45fcaa02df41309ca3a3dbea4
Neko <40058557+neko-js@users.noreply.github.com>
2023-07-19 01:47:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -48,3 +48,9 @@ docker run hello-world
 
 # Start Pihole
 docker compose up -d
+
+# Display Finished Message
+echo ""
+echo "Installation is finished."
+echo "Set your device DNS to $(hostname -I | cut -d' ' -f1)"
+echo "You can reach the UI under: http://$(hostname -I | cut -d' ' -f1)/admin"
