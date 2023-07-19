a11620516a406d34aaa2c71278d5a18ffb574c5a
Neko <40058557+neko-js@users.noreply.github.com>
2023-07-19 02:22:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,25 +37,4 @@ echo "Samba configuration finished."
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
-# Docker Installation Sets us into root user
-su - pi -c '
-    # Post Docker Steps
-    sudo groupadd docker
-    sudo usermod -aG docker $USER
-    newgrp docker
-    sudo chmod 666 /var/run/docker.sock
-    
-    # Verify Installation
-    docker -v
-    docker run hello-world
-    
-    # Start Pihole
-    docker compose up -d
-    
-    # Display Finished Message
-    echo ""
-    echo "Installation is finished."
-    echo "Set your device DNS to $(hostname -I | cut -d' ' -f1)"
-    echo "You can reach the UI under: http://$(hostname -I | cut -d' ' -f1)/admin"'
-
 exit
